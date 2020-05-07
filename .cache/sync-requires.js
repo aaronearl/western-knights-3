e4a234c06cdf8342cf4d29db13e1baac9d072f00
74thstreet <aaron.earl.harris@me.com>
2020-05-06 22:58:59 -0700

    added disqus

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/user/Desktop/Dev_Folder/western-knights-3/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/user/Desktop/Dev_Folder/western-knights-3/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/user/Desktop/Dev_Folder/western-knights-3/src/pages/index.js"))),
   "component---src-pages-success-js": hot(preferDefault(require("/Users/user/Desktop/Dev_Folder/western-knights-3/src/pages/success.js")))
 }
