e69c46b0efb8125499872c8259fe61d6f475806a
Aaron Harris <Aaron.earl.harris@me.com>
2022-03-22 20:54:16 -0700

    pushed repo for review by Phil, errors exist, gatsby-image is broken

--- src/components/HeroText.js
+++ src/components/HeroText.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Typist from 'react-typist'
+// import Typist from 'react-typist'
 
 import { media } from '../utils/style'
 
