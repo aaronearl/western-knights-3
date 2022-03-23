97d2e13b61b716843400642bdb67cde29b019ad2
Aaron Harris <Aaron.earl.harris@me.com>
2022-03-23 11:08:06 -0700

    aaded gatsby-plugin-image into gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,5 +118,8 @@ module.exports = {
     // To learn more, visit: https://gatsby.app/offline
     "gatsby-plugin-offline",
     `gatsby-plugin-stripe`,
+    `gatsby-plugin-image`,
+    // `gatsby-plugin-sharp`,
+    // `gatsby-transformer-sharp`
   ],
 };
